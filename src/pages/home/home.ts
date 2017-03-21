89c41fc624f4227c41bb5cf46e328c2dc24547bf
Fadlika Dita Nurjanto <fadlika@kidsoft.com.au>
2017-03-21 13:18:14 +0700

    Simple Form

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public details: any = {
+    name: 'James Griffiths',
+    occupation: 'Software Engineer',
+    platform: 'Other'
+  };
+
   constructor(public navCtrl: NavController) {
     
+  }
+
+  saveDetails() {
+    console.log(`My name is ${this.details.name}`);
+    console.log(`I work as a ${this.details.occupation}`);
+    console.log(`My favourite platform is ${this.details.platform}`);
   }
 
 }
